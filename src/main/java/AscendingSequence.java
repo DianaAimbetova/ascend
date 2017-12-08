fd65068d668d4895dd6a56bb09382f9c6761abdc
Diana_Aimbetova <diana_aimbetova@epam.com>
2017-12-08 14:33:01 +0600

    попытка 2.0

--- src/main/java/AscendingSequence.java
+++ src/main/java/AscendingSequence.java
@@ -8,15 +8,32 @@ public class AscendingSequence {
 
     }
 
-    public static boolean isAscendingSequence(int number){
-        String intToStr = Integer.toString(number);
-        int length = intToStr.length();
-        for(int i = 1; i < length; i++){
-                if ((intToStr.charAt(i) > intToStr.charAt((i - 1))) ) {
-                    return true;
-                }
-            }
 
+    public static boolean isAscendingSequence(int num) {
+        int lastNumber = num%10;
+        //System.out.println("последняя цифра: " + lastNumber);
+        int remain = num/10;
+      //  System.out.println("остаток без последней цифры: " + remain);
+        boolean ascend = false;
+        boolean descend = false;
+        while(remain>0){
+            int currentNumber = remain%10;
+            //System.out.println("предпоследняя цифра: " + currentNumber);
+            if(currentNumber > lastNumber) {
+              //  System.out.println("if1");
+                descend = true;
+            } else if(currentNumber < lastNumber) {
+             //   System.out.println("if2");
+                ascend = true;
+            }
+            lastNumber = currentNumber;
+            //System.out.println(lastNumber);
+            remain /= 10;
+            //System.out.println(remain);
+        }
+        if(ascend && !descend) {
+            return true;
+        }
         return false;
 
     }
@@ -25,6 +42,7 @@ public class AscendingSequence {
     public static void main(String[] args){
         try{
         Scanner reader = new Scanner(System.in);
+
         System.out.println("Введите число: ");
         int number = reader.nextInt();
         if((number>=0)) {
@@ -32,7 +50,7 @@ public class AscendingSequence {
             if(Integer.toString(number).length()>2){
             if(isAscendingSequence(number)){
                 System.out.println("Образуют");
-            }else{
+            }else if(!isAscendingSequence(number)){
                 System.out.println("Не образуют");
             }
             }else{
